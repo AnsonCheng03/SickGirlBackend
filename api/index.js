daa5e3c48e84ed883e54eac198fefbb338cc56b5
Anson Cheng <Anson.12666@icloud.com>
2025-02-20 23:19:37 +0800

    chore: add ws server

--- api/index.js
+++ api/index.js
@@ -1,8 +1,34 @@
 const express = require("express");
+const SocketServer = require("ws").Server;
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
+
+const wss = new SocketServer({ server });
+//當有 client 連線成功時
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+  // 當收到client消息時
+  ws.on("message", (data) => {
+    // 收回來是 Buffer 格式、需轉成字串
+    data = data.toString();
+    console.log(data); // 可在 terminal 看收到的訊息
+
+    /// 發送消息給client
+    ws.send(data);
+
+    /// 發送給所有client：
+    let clients = wss.clients; //取得所有連接中的 client
+    clients.forEach((client) => {
+      client.send(data); // 發送至每個 client
+    });
+  });
+  // 當連線關閉
+  ws.on("close", () => {
+    console.log("Close connected");
+  });
+});
 
 const createThread = async () => {
   // send POST request to create thread https://api.openai.com/v1/threads
@@ -105,7 +131,7 @@ const createRun = async (userInput, res) => {
             onEvent(currentEvent, data);
           }
         } catch (error) {
-          console.error("Error parsing JSON:", error, line);
+          console.log("Error parsing JSON:", error, line);
         }
       }
     });
